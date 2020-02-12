5fe7d2a505030ba902b397485f91c068fdcd6af6
Ravil Galeyev <rgaleyev@gmail.com>
2020-02-12 22:21:03 +0100

    ZL-2 Add DSL for Comparable (#1)

---
+++ src/main/java/me/dehasi/zeliba/TheComparable.java
@@ -0,0 +1,42 @@
+package me.dehasi.zeliba;
+
+public class TheComparable<T extends Comparable<T>> {
+
+    private final T value;
+
+    private TheComparable(T value) {
+        this.value = value;
+    }
+
+    public static <T extends Comparable<T>> TheComparable<T> a(T value) {
+        return the(value);
+    }
+
+    public static <T extends Comparable<T>> TheComparable<T> an(T value) {
+        return the(value);
+    }
+
+    public static <T extends Comparable<T>> TheComparable<T> the(T value) {
+        return new TheComparable<>(value);
+    }
+
+    public boolean isEqualsTo(T that) {
+        return value.compareTo(that) == 0;
+    }
+
+    public boolean isGreaterThan(T that) {
+        return value.compareTo(that) > 0;
+    }
+
+    public boolean isGreaterOrEqualsThan(T that) {
+        return value.compareTo(that) >= 0;
+    }
+
+    public boolean isLessThan(T that) {
+        return value.compareTo(that) < 0;
+    }
+
+    public boolean isLessOrEqualsThan(T that) {
+        return value.compareTo(that) <= 0;
+    }
+}
---
+++ src/test/java/me/dehasi/zeliba/TheComparableTest.java
@@ -0,0 +1,53 @@
+package me.dehasi.zeliba;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static java.math.BigDecimal.TEN;
+import static me.dehasi.zeliba.TheComparable.a;
+import static me.dehasi.zeliba.TheComparable.the;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TheComparableTest {
+
+    @MethodSource("bigDecimals")
+    @ParameterizedTest void isEqualsTo(BigDecimal val) {
+        assertTrue(a(val).isEqualsTo(val));
+    }
+
+    @MethodSource("bigDecimals")
+    @ParameterizedTest void isGreaterThan(BigDecimal val) {
+        assertTrue(the(val.multiply(TEN)).isGreaterThan(val));
+    }
+
+    @MethodSource("bigDecimals")
+    @ParameterizedTest void isGreaterOrEqualsThan(BigDecimal val) {
+        assertTrue(the(val).isGreaterOrEqualsThan(val));
+        assertTrue(the(val.multiply(TEN)).isGreaterOrEqualsThan(val));
+    }
+
+    @MethodSource("bigDecimals")
+    @ParameterizedTest void isLessThan(BigDecimal val) {
+        assertTrue(the(val).isLessThan(val.multiply(TEN)));
+    }
+
+    @MethodSource("bigDecimals")
+    @ParameterizedTest void isLessOrEqualsThan(BigDecimal val) {
+        assertTrue(the(val).isLessOrEqualsThan(val));
+        assertTrue(the(val).isLessOrEqualsThan(val.multiply(TEN)));
+    }
+
+    private static Stream<Arguments> bigDecimals() {
+        return Stream.generate(() -> Arguments.of(random(42))).limit(40);
+    }
+
+    private static BigDecimal random(int range) {
+        BigDecimal max = new BigDecimal(range);
+        BigDecimal randFromDouble = new BigDecimal(Math.random());
+        BigDecimal actualRandomDec = randFromDouble.multiply(max);
+        return actualRandomDec.setScale(2, BigDecimal.ROUND_DOWN);
+    }
+}
