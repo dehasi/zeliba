241c5afe1b840792dfeb6f208590803e16fecfc0
Ravil Galeyev <dehasi@yandex.ru>
2020-02-18 20:16:21 +0100

    ZL-10 Add map support

---
+++ src/main/java/me/dehasi/zeliba/TheMap.java
@@ -0,0 +1,40 @@
+package me.dehasi.zeliba;
+
+import java.util.Map;
+
+public class TheMap<MAP extends Map<?,?>> {
+
+    private final MAP map;
+
+    private TheMap(MAP map) {
+        this.map = map;
+    }
+
+    public static <MAP extends Map<?,?>> TheMap a(MAP value) {
+        return the(value);
+    }
+
+    public static <MAP extends Map<?,?>> TheMap an(MAP value) {
+        return the(value);
+    }
+
+    public static <MAP extends Map<?,?>> TheMap the(MAP value) {
+        return new TheMap<>(value);
+    }
+
+    public boolean isNull() {
+        return map == null;
+    }
+
+    public boolean isNullOrEmpty() {
+        return isNull() || map.isEmpty();
+    }
+
+    public boolean isNotNull() {
+        return !isNull();
+    }
+
+    public boolean isNotEmpty() {
+        return !map.isEmpty();
+    }
+}
