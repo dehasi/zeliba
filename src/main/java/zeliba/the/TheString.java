588e11db28565dafa6c45f3490f49e4248a58935
Ravil Galeyev <dehasi@yandex.ru>
2021-02-10 21:00:16 +0100

    ZL-26 Add the(string)

--- src/main/java/zeliba/the/TheString.java
+++ src/main/java/zeliba/the/TheString.java
@@ -1,27 +1,29 @@
 package zeliba.the;
 
+import static java.util.Objects.requireNonNull;
+
 class TheString {
 
     private final String string;
 
-    private TheString(String string) {this.string = string;}
+    private TheString(String string) {
+        this.string = requireNonNull(string);
+    }
 
     public static TheString the(String string) {
         return new TheString(string);
     }
 
-    public String substring(int beginIndex, int endIndex) {
-        if (beginIndex < 0) {
-            beginIndex = 0;
-        }
-        if (endIndex > string.length()) {
-            endIndex = string.length();
-        }
-        int subLen = endIndex - beginIndex;
-        if (subLen < 0) {
-            return "";
-        }
+    public String substring(int fromIncluded, int toExluded) {
+        if (fromIncluded < 0)
+            fromIncluded = 0;
 
-        return string.substring(beginIndex, endIndex);
+        if (toExluded > string.length())
+            toExluded = string.length();
+
+        if (toExluded - fromIncluded < 0)
+            return "";
+
+        return string.substring(fromIncluded, toExluded);
     }
 }
